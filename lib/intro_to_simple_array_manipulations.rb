67efb7cf97ada2af4e66d99d0aa957cd769020fd
Kristian Portillo <portillk@gmail.com>
2020-07-06 18:06:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,7 +14,7 @@ def using_pop(continents)
     deleted_string = continents.pop
 end
 
-def using_pop_with_args(dog_breeds)
+def pop_with_args(dog_breeds)
     dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
     small_dogs = dog_breeds.pop(2)
 end
