80ac56d4fb32e85d64b11a9e8895fd7747d84109
Kristian Portillo <portillk@gmail.com>
2020-07-06 18:08:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,7 +10,7 @@ def using_unshift(bouroughs_in_nyc,new_neighborhood)
     bouroughs_in_nyc.unshift(new_neighborhood)
 end
 def using_pop(continents)
-    continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+   # continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
     deleted_string = continents.pop
 end
 
@@ -20,7 +20,7 @@ def pop_with_args(dog_breeds)
 end
 
 def using_shift(my_favorite_cities) 
-    my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+   # my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
     im_so_over_this_city = my_favorite_cities.shift
 end 
 
