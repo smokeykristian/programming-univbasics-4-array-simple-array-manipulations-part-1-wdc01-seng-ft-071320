7b71f186514a606a7b2201b00eed30f52ac60307
Kristian Portillo <portillk@gmail.com>
2020-07-06 18:04:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,7 +17,7 @@ end
 def using_pop_with_args(dog_breeds)
     dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
     small_dogs = using_pop_with_args.pop(2)
-
+end
 
 def using_shift 
     my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
